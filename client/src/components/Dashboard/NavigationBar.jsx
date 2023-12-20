a6fc55dc02c328cb243ffcb4f3999b0fa0d9e8b7
Harresh Yohan <harresh.baptist@betmakers.com>
2023-12-20 13:45:03 +0530

    logout function

---
+++ client/src/components/Dashboard/NavigationBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const NavigationBar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+};
